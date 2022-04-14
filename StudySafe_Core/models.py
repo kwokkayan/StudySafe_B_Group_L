19cfdf4f454b080a469f0409d00bb243746abd22
htchan3 <chthilda@connect.hku.hk>
2022-04-14 20:54:40 +0800

    Venue model created

--- StudySafe_Core/models.py
+++ StudySafe_Core/models.py
@@ -1,3 +1,22 @@
+#from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+class Venues(models.Model):
+        Venue_Code = models.CharField(max_length=20, unique=True,db_column='Venue Code')
+        
+        Location = models.CharField(max_length=150)
+
+        ROOM_TYPE = (
+                ('LT','LT'),
+                ('CR','CR'),
+                ('TR','TR')
+        )
+        Type = models.CharField(max_length=2, choices=ROOM_TYPE)
+
+        Capacity = models.IntegerField()
+
+        def __str__(self):
+                return self.VenueCode
+
 
 # Create your models here.
