734e221b9eaab798939dacdd67ced5a087838099
kwokkayan <kwokkayan0712@gmail.com>
2022-04-14 15:53:15 +0800

    Update urls.py

--- StudySafe_Core/urls.py
+++ StudySafe_Core/urls.py
@@ -1,11 +1,10 @@
 from email.policy import default
-import imp
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from StudySafe_Core import views, hkumember_api_view
 
 router = DefaultRouter()
-router.register(r'hkumembers', hkumember_api_view.ViewAllHKUMembers, 'view-all-HKUMembers')
+router.register(r'hkumembers', hkumember_api_view.HKUMemberViewSet, 'view-all-HKUMembers')
 
 urlpatterns = [
     path('api/', include(router.urls))
