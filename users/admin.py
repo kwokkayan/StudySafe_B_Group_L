3e43b81e8aac24f02c27c0acb52fb94b45477c4f
Samson Lam <samlamson2001@gmail.com>
2022-04-15 11:47:39 +0800

    updated admin.py

--- users/admin.py
+++ users/admin.py
@@ -5,4 +5,18 @@ from django.contrib.auth.admin import UserAdmin
 from .users import ChangeDeviceForm, ChangeMemberForm, CreateDeviceForm, CreateMemberForm
 from .models import deviceAccounts, MemberAccounts
 
-#pls help help here, don't really understand
+class DeviceAdmin (UserAdmin):
+    add_form = CreateDeviceForm
+    form = ChangeDeviceForm
+    model = deviceAccounts
+    list_display = ['username', ]
+
+class MemberAdmin (UserAdmin):
+    add_form = CreateMemberForm
+    form = ChangeDeviceForm
+    model = MemberAccounts
+    list_display = ['username', 'first_name', 'last_name', 'emailAddress',]
+
+
+admin.site.register (deviceAccounts, DeviceAdmin )
+admin.site.register (MemberAccounts, MemberAdmin)
