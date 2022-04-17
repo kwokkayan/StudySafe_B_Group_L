18b3d0c9800bd140a2cd74fb62c10faf469e0299
kwokkayan <kwokkayan0712@gmail.com>
2022-04-17 23:31:10 +0800

    deploy files

--- admin/settings.py
+++ admin/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://studysafe-b-group-l.herokuapp.com/', '127.0.0.1', 'localhost']
 
 
 # Application definition
