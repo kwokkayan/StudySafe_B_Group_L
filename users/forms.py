6769967f956df73c2d2809ebccd4b8b44719ce82
kwokkayan <kwokkayan0712@gmail.com>
2022-04-17 19:54:48 +0800

    more minor fixes
    removed "comments" with multi lined strings.
    changed meta to Meta in form class declarations.

--- users/forms.py
+++ users/forms.py
@@ -9,33 +9,12 @@ from django.core.validators import EMPTY_VALUES
 
 class CreateUser (UserCreationForm):
     
-    class meta:
+    class Meta:
         model = TaskforceAccount
         fields = ('username', 'role_type')
 
 class ChangeUser (UserChangeForm):
-    class meta:
+    class Meta:
         model = TaskforceAccount
         fields = ('username', 'role_type')
 
-"""
-class DeviceCreation (ModelForm):
-    class Meta:
-        model = DeviceAccounts
-        fields = ('__all__')
-
-class MemberCreation (ModelForm):
-    class Meta:
-        model = MemberAccounts
-        fields = ('__all__')
-
-class DeviceChange (ModelForm):
-    class Meta:
-        model = DeviceAccounts
-        fields = ('__all__')
-        
-class MemberChange (ModelForm):
-    class Meta:
-        model = MemberAccounts
-        fields = ('__all__')
-"""
--- users/models.py
+++ users/models.py
@@ -22,25 +22,3 @@ class TaskforceAccount(AbstractUser):
     def __str__(self):
         return "Username: {} User Type: {}".format(self.username, self.role_type)
 
-"""
-class DeviceAccounts (models.Model):
-    user = models.OneToOneField(User, related_name='device_account', on_delete=models.CASCADE, primary_key=True)
-    def save(self, *args, **kwargs):
-        if self.user.role_type == 'Member':
-            return
-        super().save(*args, **kwargs)
-    def __str__ (self):
-        return self.username
-
-class MemberAccounts (models.Model):
-    user = models.OneToOneField(User, related_name='member_account', on_delete=models.CASCADE, primary_key=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    emailAddress = models.EmailField(verbose_name="email", max_length=60, unique=True)
-    def save(self, *args, **kwargs):
-        if self.user.role_type == 'Device':
-            return
-        super().save(*args, **kwargs)
-    def __str__(self) :
-        return self.username
-"""
