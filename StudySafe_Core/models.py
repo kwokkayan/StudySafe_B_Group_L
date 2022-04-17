0d26a634a73ae63c0a945afb19db62654a5e814d
kwokkayan <kky0712@connect.hku.hk>
2022-04-17 16:07:56 +0800

    Update StudySafe_Core/models.py
    Co-authored-by: kevinShu <44152755+r0ngk@users.noreply.github.com>

--- StudySafe_Core/models.py
+++ StudySafe_Core/models.py
@@ -24,7 +24,7 @@ class HKUMember(models.Model):
     name = models.CharField(max_length=150)
     uid  = models.CharField(max_length=10, primary_key=True)
     def __str__(self):
-        return 'Name: {}, UID: {}'.format(self.uid, self.name)
+        return 'Name: {}, UID: {}'.format(self.name, self.uid)
 
 class TravelRecord(models.Model):
     venue_code =  models.ForeignKey('Venues', on_delete=models.CASCADE)
