d1ebb595df7225f32f502e7cc84a93871ecd5858
kwokkayan <kwokkayan0712@gmail.com>
2022-04-17 19:52:07 +0800

    minor fixes
    fixed default role value.
    added string rep for user record.

--- users/models.py
+++ users/models.py
@@ -11,7 +11,7 @@ class TaskforceAccount(AbstractUser):
         ('Device', 'Device'),
         ('Member', 'Member')
     )
-    role_type = models.CharField(max_length=10, choices=ROLES, default=1)
+    role_type = models.CharField(max_length=10, choices=ROLES, default='Device')
     def clean (self):
         if self.role_type == 'Member' and (self.email in EMPTY_VALUES or self.first_name in EMPTY_VALUES or self.last_name in EMPTY_VALUES):
             raise ValidationError ({
@@ -19,6 +19,8 @@ class TaskforceAccount(AbstractUser):
                 'first_name': ValidationError(("Missing first_name"), code='required'),
                 'last_name': ValidationError(("Missing last_name"), code='required')
             })
+    def __str__(self):
+        return "Username: {} User Type: {}".format(self.username, self.role_type)
 
 """
 class DeviceAccounts (models.Model):
