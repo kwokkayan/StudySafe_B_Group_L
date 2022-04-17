076a8f1fa0019982a6e2ee663d1cec7628282263
kwokkayan <kwokkayan0712@gmail.com>
2022-04-18 00:32:12 +0800

    changed hostname

--- admin/settings.py
+++ admin/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str("DJANGO_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://studysafe-b-group-l.herokuapp.com/', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['studysafe-b-group-l.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
