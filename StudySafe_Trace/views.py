53b74db1f24da7d5bdc0bce796650b27eca9dd18
kwokkayan <kwokkayan0712@gmail.com>
2022-04-25 19:18:41 +0800

    added venues logic
    need testing tho

--- StudySafe_Trace/views.py
+++ StudySafe_Trace/views.py
@@ -1,10 +1,10 @@
 import requests
+from datetime import datetime 
 from django.views.generic import TemplateView
 # Create your views here.
 class BaseView(TemplateView):
     def dispatch(self, request, *args, **kwargs):
         self.meta = request.META
-        print(self.meta)
         return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
@@ -33,6 +33,19 @@ class VenuesView(BaseView):
         context = super().get_context_data(**kwargs)
         if context["user_found"]:
             # add venues logic
-            print(self.result)
-            pass
+            visited = self.result["visited"]
+            context["venues"] = []
+            for v in visited:
+                if v["time_of_exit"] is None:
+                    continue
+                time_of_entry = datetime.fromisoformat(v["time_of_entry"])
+                # check entry and onset dates
+                ddate = context["date"] - time_of_entry.date()
+                print(ddate)
+                if 0 <= ddate.days <= 2:
+                    time_of_exit = datetime.fromisoformat(v["time_of_exit"])
+                    dt = time_of_exit - time_of_entry
+                    if dt.total_seconds() >= 1800: # 30 mins
+                        context["venues"].append(v["venue_code"])
+            context["venues"].sort()
         return context
